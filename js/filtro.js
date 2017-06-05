03713bc701a01470d6168f592eeabb3050ac8196
lucastvasconcelos <lucastavaresvasconcelos@gmail.com>
2017-06-05 07:01:19 -0300

    adicionando filtro de paciente(com regex)

--- js/filtro.js
+++ js/filtro.js
@@ -1,13 +1,21 @@
 receberFiltro = document.querySelector("#filtrar-tabela");
 receberFiltro.addEventListener("input",function() {
     var pacientes = document.querySelectorAll(".paciente")
+
     for (paciente of pacientes){
-    var tdNome =  paciente.querySelector(".info-nome")
-    var nome = tdNome.textContent
-    if(nome != receberFiltro.value){
-        paciente.classList.add("invisivel")
+        var tdNome =  paciente.querySelector(".info-nome")
+        var nome = tdNome.textContent
+        var expressao = new RegExp(this.value,"i")
+        if(!expressao.test(nome)){
+            paciente.classList.add("invisivel")
+        }
+        else{
+            paciente.classList.remove("invisivel")
+        }
     }
-    else{
+    if(receberFiltro.value.length == 0){
+        for (paciente of pacientes){
         paciente.classList.remove("invisivel")
+        }    
     }
-}})
+})
